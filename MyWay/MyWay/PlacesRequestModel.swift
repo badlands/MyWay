acd442142a4cc3b29d3b45155d07ecae896f2481
Marco <badlands@email.it>
2016-06-30 23:58:47 +0200

    added basic pods (Alamofire, Realm, Mantle), completed first setup of the project

--- MyWay/MyWay/PlacesRequestModel.swift
+++ MyWay/MyWay/PlacesRequestModel.swift
@@ -6,4 +6,20 @@
 //  Copyright © 2016 Marco Marengo. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import CoreLocation
+import Mantle
+
+class PlacesRequestModel : MTLModel, MTLJSONSerializing {
+    private(set) var query : String = ""
+    private(set) var location : String = ""
+    
+
+    static func JSONKeyPathsByPropertyKey() -> [NSObject : AnyObject]! {
+        return [
+            "query" : "q",
+            "location" : "at"
+        ]
+    }
+
+}
--- MyWay/MyWay/ViewController.swift
+++ MyWay/MyWay/ViewController.swift
@@ -13,6 +13,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let r = try! PlacesRequestModel(dictionary: [ "location" : "1234,12345", "query" : "Berlin "])
+        print(r)
     }
 
     override func didReceiveMemoryWarning() {
