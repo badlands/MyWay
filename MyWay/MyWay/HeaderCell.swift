3e95bc5eef7eacefe4356b20dfea8e802c5ca604
Marco Marengo <marco.marengo@gmail.com>
2016-07-04 16:46:11 +0200

    added app icon + start image

---
+++ MyWay/MyWay/HeaderCell.swift
@@ -0,0 +1,24 @@
+//
+//  HeaderCell.swift
+//  MyWay
+//
+//  Created by Marco Marengo on 04/07/16.
+//  Copyright © 2016 Marco Marengo. All rights reserved.
+//
+
+import UIKit
+
+class HeaderCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
